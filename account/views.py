44e67f8c0996d33a1f6ce6a8c04cf64d3766856d
raktim707 <raktimahmed2019@outlook.com>
2021-07-15 20:07:47 +0600

    initial commit

---
+++ account/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .forms import UserRegistrationForm,UserEditForm, ProfileEditForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from common.decorators import ajax_required
+from django.views.decorators.http import require_POST
+from django.contrib import messages
+from .models import Profile, Contact
+from actions.utils import create_action
+from actions.models import Action
+
+@login_required
+def dashboard(request):
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions': actions})
+
+def register(request):
+    if request.method=='POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            create_action(request.user, 'has created an account')
+            return render(request, 'account/register_done.html', {'new_user':new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Profile updated succesfully")
+        else:
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form':profile_form})
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active = True)
+    return render(request, 'account/user/list.html', {'section':'people', 'users':users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username = username, is_active=True)
+    return render(request, 'account/user/detail.html', {'section': 'people', 'user':user})
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'error'})
+    return JsonResponse({'status':'error'})
---
+++ images/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Image
+from urllib import request
+from django.utils.text import slugify
+from django.core.files.base import ContentFile
+
+class ImageCreateForm(forms.ModelForm):
+
+    class Meta:
+        model = Image
+        fields = ('title', 'url', 'description')
+        widgets = {
+            'url': forms.HiddenInput,
+        }
+    
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extension = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extension:
+            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        return url
+    
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+
+        if commit:
+            image.save()
+        return image
+
